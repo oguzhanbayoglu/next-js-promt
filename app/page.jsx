9a7b4ef539a4b305fa196089cee7fa60fada7b90
oguzhanbayoglu <oguzhanbayoglu1@gmail.com>
2023-06-11 15:39:23 +0300

    initial

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <section className="w-full flex-center- flex-col">
+      <h1 className="head_text text-center font-montserrat ">
+        Discover & Share
+      </h1>
+      <h2 className="text-[3rem] font-black orange_gradient text-center">
+        AI Powered Promts
+      </h2>
+    </section>
+  );
+};
+
+export default Home;
