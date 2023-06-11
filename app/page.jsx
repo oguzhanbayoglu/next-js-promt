b90ea3aded9c3f5e75b599f8ac0b288e5dd37e60
oguzhanbayoglu <oguzhanbayoglu1@gmail.com>
2023-06-11 15:48:45 +0300

    commit

--- app/page.jsx
+++ app/page.jsx
@@ -7,6 +7,10 @@ const Home = () => {
       <h2 className="text-[3rem] font-black orange_gradient text-center">
         AI Powered Promts
       </h2>
+      <p className="desc text-center">
+        This is a project that is made to help me to understand the basics of
+        the Next js framework
+      </p>
     </section>
   );
 };
